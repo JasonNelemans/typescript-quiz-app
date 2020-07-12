6e6a25b6efae00ab30349c618a3361f56584313b
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-12 13:52:37 +0200

    created shuffleArray to shuffle questions answers

--- src/API.ts
+++ src/API.ts
@@ -1,3 +1,5 @@
+import { shuffleArray } from './utils';
+
 export type Question = {
   category: string;
   correct_answer: string;
@@ -18,5 +20,10 @@ export enum Difficulty {
 export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty) => {
   const endpoint = `https://opentdb.com/api.php?amount=${amount}&difficulty=${difficulty}&type=multiple`;
   const data = await (await fetch(endpoint)).json();
-  console.log(data);
+  return data.results.map((question: Question) => (
+    {
+      ...question,
+      answer: shuffleArray([...question.incorrect_answer, question.correct_answer])
+    }
+  ))
 }
