827fbf36c0536567255868a5ec0ff53e4bc968d5
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-12 14:00:22 +0200

    import QuestionState in App

--- src/API.ts
+++ src/API.ts
@@ -4,7 +4,7 @@ export type Question = {
   category: string;
   correct_answer: string;
   difficutly: string;
-  incorrect_answer: string[];
+  incorrect_answers: string[];
   question: string;
   type: string;
 }
@@ -23,7 +23,7 @@ export const fetchQuizQuestions = async (amount: number, difficulty: Difficulty)
   return data.results.map((question: Question) => (
     {
       ...question,
-      answer: shuffleArray([...question.incorrect_answer, question.correct_answer])
+      answers: shuffleArray([...question.incorrect_answers, question.correct_answer])
     }
   ))
 }
