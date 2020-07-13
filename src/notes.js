1af503dbcafb1b9197bca5f37d98f04c2367bc21
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-13 14:29:20 +0200

    imported global styling to App.tsx

--- src/notes.js
+++ src/notes.js
@@ -1,3 +1,9 @@
 /* 
 Resume at 52:38
+
+In this project I added the font link to the index.html head by importing it from Google fonts. 
+
+In this project I used createGlobalStyles from styled-components. Look at how I render a background 
+image and what CSS properties I use for the global styles. 
+I import the GlobalStyles component in App.tsx and just render it at the top of the return value. 
 */
