e00277555889a62c43c2187b7f4732721aeba366
JasonNelemans <jasonnelemans@hotmail.com>
2020-07-16 22:41:43 +0200

    updated Wrapper style

--- src/notes.js
+++ src/notes.js
@@ -7,4 +7,12 @@ image and what CSS properties I use for the global styles.
 I import the GlobalStyles component in App.tsx and just render it at the top of the return value. 
 
 > p CSS selector selects all the p tags inside that div/styled component, and nothing outside of it. 
+
+Further study these:
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
+    -moz-background-clip: text;
+    -moz-text-fill-color: transparant;
+
+
 */
